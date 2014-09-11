604f2f37943cb73f66b14fe7b940cc0b00478243
Vikram Hakkal <vhakkal@gmail.com>
2014-09-11 19:36:27 -0400

    Class 3 Lab 3

--- bookstore/app/controllers/books_controller.rb
+++ bookstore/app/controllers/books_controller.rb
@@ -7,4 +7,21 @@ class BooksController < ApplicationController
 	def show
 		@book = Book.find(params[:id])
 	end
+
+	def new
+		@book = Book.new
+	end
+
+	def create
+		@book = Book.new(book_params)
+		@book.save
+		redirect_to @book
+	end
+
+	private
+
+	def book_params
+		params.require(:book).permit(:title, :author, :pages, :price)
+	end
+
 end
