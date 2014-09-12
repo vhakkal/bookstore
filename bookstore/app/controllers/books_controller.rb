495775eb92ce26d82fb8fd7d6d214dff10668cc2
Vikram Hakkal <vhakkal@gmail.com>
2014-09-11 20:43:43 -0400

    Class 3 Lab 5

--- bookstore/app/controllers/books_controller.rb
+++ bookstore/app/controllers/books_controller.rb
@@ -1,11 +1,12 @@
 class BooksController < ApplicationController
+	before_action :set_book, only:[ :show, :edit, :update, :destroy]
+
 	def index
 		@avilable_at = Time.now
 		@books = Book.all
 	end
 
 	def show
-		@book = Book.find(params[:id])
 	end
 
 	def new
@@ -19,19 +20,25 @@ class BooksController < ApplicationController
 	end
 
 	def edit
-		@book = Book.find(params[:id])
 	end
 
 	def update
-		@book = Book.find(params[:id])
 		@book.update(book_params)
 		redirect_to @book
 	end
-	
+
+	def destroy
+	 	@book.destroy
+	 	redirect_to books_url
+	end
+
 	private
 
 	def book_params
 		params.require(:book).permit(:title, :author, :pages, :price)
 	end
 
+	def set_book
+		@book = Book.find(params[:id])
+	end
 end
