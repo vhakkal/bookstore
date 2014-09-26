80bc2d6979dea31f4f9d231a395f44db1545d28b
Vikram Hakkal <vhakkal@gmail.com>
2014-09-25 22:12:11 -0400

    Class4 - Lab 3

--- bookstore/app/controllers/books_controller.rb
+++ bookstore/app/controllers/books_controller.rb
@@ -15,16 +15,22 @@ class BooksController < ApplicationController
 
 	def create
 		@book = Book.new(book_params)
-		@book.save
-		redirect_to @book
+		if @book.save
+			redirect_to @book
+		else
+			render :new
+		end
 	end
 
 	def edit
 	end
 
 	def update
-		@book.update(book_params)
-		redirect_to @book
+		if @book.update(book_params)
+			redirect_to @book
+		else
+			render :new
+		end
 	end
 
 	def destroy
